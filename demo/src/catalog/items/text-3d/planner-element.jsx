d0b940b70594571b962724415a20e3de171d63e6
Vikms95 <vms1995@hotmail.com>
2022-12-21 12:45:32 +0100

    check if remote is created

--- demo/src/catalog/items/text-3d/planner-element.jsx
+++ demo/src/catalog/items/text-3d/planner-element.jsx
@@ -1,9 +1,10 @@
-import { FontLoader, TextGeometry, MeshBasicMaterial, Mesh, BoxHelper } from 'three';
+import { TextGeometry, MeshBasicMaterial, Mesh, BoxHelper } from 'three';
 import React from 'react';
 import { HELVETIKER } from './helvetiker_regular.typeface.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
 const fontLoader = new FontLoader();
-const font = fontLoader.parse(HELVETIKER);
+const font = fontLoader.parse( HELVETIKER );
 
 const defaultFontSize = 16;
 const defaultColor = '#000000';
@@ -13,10 +14,10 @@ export default {
   prototype: 'items',
 
   info: {
-    tag: ['text'],
+    tag: [ 'text' ],
     title: 'Text 3D',
     description: 'Text',
-    image: require('./img.png')
+    image: require( './img.png' )
   },
 
   properties: {
@@ -45,11 +46,11 @@ export default {
     }
   },
 
-  render2D: function (element, layer, scene) {
+  render2D: function ( element, layer, scene ) {
 
-    let color = element.properties.get('color') || defaultColor;
-    let text = element.properties.get('text') || '';
-    let fontSize = element.properties.get('fontSize') || defaultFontSize;
+    let color = element.properties.get( 'color' ) || defaultColor;
+    let text = element.properties.get( 'text' ) || '';
+    let fontSize = element.properties.get( 'fontSize' ) || defaultFontSize;
     let textHorizontalPadding = defaultFontSize;
     let width = ( ( text.length - ( text.length / 2 ) ) * fontSize ) + textHorizontalPadding;
     let height = 2 * fontSize;
@@ -57,11 +58,11 @@ export default {
     return (
       <g>
         <rect
-          x={-width / 2}
-          y={-height / 2}
+          x={ -width / 2 }
+          y={ -height / 2 }
           fill="#FFF"
-          width={width}
-          height={height}
+          width={ width }
+          height={ height }
           stroke="#000"
           strokeWidth="2"
         />
@@ -71,38 +72,38 @@ export default {
           fontFamily="Arial"
           alignmentBaseline="middle"
           textAnchor="middle"
-          fontSize={fontSize}
-          fill={color}
-          transform={'scale(1,-1)'}
+          fontSize={ fontSize }
+          fill={ color }
+          transform={ 'scale(1,-1)' }
         >
-          {text}
+          { text }
         </text>
       </g>
-    )
+    );
   },
 
-  render3D: function (element, layer, scene) {
+  render3D: function ( element, layer, scene ) {
 
-    let text = element.properties.get('text') || '';
-    let size = element.properties.get('fontSize') || defaultFontSize;
+    let text = element.properties.get( 'text' ) || '';
+    let size = element.properties.get( 'fontSize' ) || defaultFontSize;
     let textHorizontalPadding = defaultFontSize;
     let width = ( ( text.length - ( text.length / 2 ) ) * size ) + textHorizontalPadding;
-    let color = element.properties.get('color') || defaultColor;
+    let color = element.properties.get( 'color' ) || defaultColor;
 
     let mesh = new Mesh(
-      new TextGeometry( text, { size, height: 1, font }),
-      new MeshBasicMaterial({color})
+      new TextGeometry( text, { size, height: 1, font } ),
+      new MeshBasicMaterial( { color } )
     );
 
-    if (element.selected) {
-      let box = new BoxHelper(mesh, 0x99c3fb);
+    if ( element.selected ) {
+      let box = new BoxHelper( mesh, 0x99c3fb );
       box.material.linewidth = 2;
       box.material.depthTest = false;
       box.renderOrder = 1000;
-      mesh.add(box);
+      mesh.add( box );
     }
 
-    mesh.position.y += element.properties.getIn(['altitude','length']);
+    mesh.position.y += element.properties.getIn( [ 'altitude', 'length' ] );
     mesh.position.x -= ( width / 2 );
 
     return Promise.resolve( mesh );
--- src/components/viewer3d/grid-creator.js
+++ src/components/viewer3d/grid-creator.js
@@ -1,26 +1,28 @@
 import * as Three from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { HELVETIKER } from './libs/helvetiker_regular.typeface.js';
 import gridHorizontalStreak from './grids/grid-horizontal-streak';
 import gridVerticalStreak from './grids/grid-vertical-streak';
 
-export default function createGrid(state, scene) {
+
+export default function createGrid ( state, scene ) {
 
   let gridMesh = new Three.Object3D();
   gridMesh.name = 'grid';
-  let fontLoader = new Three.FontLoader();
-  let font = fontLoader.parse(HELVETIKER); // For measures
+  let fontLoader = new FontLoader();
+  let font = fontLoader.parse( HELVETIKER ); // For measures
   let { grids, width, height } = scene;
 
-  grids.forEach(grid => {
-    switch (grid.type) {
+  grids.forEach( grid => {
+    switch ( grid.type ) {
       case 'horizontal-streak':
-        gridMesh.add(gridHorizontalStreak(state, width, height, grid, font));
+        gridMesh.add( gridHorizontalStreak( state, width, height, grid, font ) );
         break;
       case 'vertical-streak':
-        gridMesh.add(gridVerticalStreak(state, width, height, grid, font));
+        gridMesh.add( gridVerticalStreak( state, width, height, grid, font ) );
         break;
     }
-  });
+  } );
 
   gridMesh.position.y = -1;
   return gridMesh;
