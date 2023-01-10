71d31671a7681b1c6b3537eae466f0a74d414f15
Vikms95 <vms1995@hotmail.com>
2023-01-10 12:40:15 +0100

    refactor fn menuPreferencias

--- src/components/menuPreferencias/menuPreferencias.jsx
+++ src/components/menuPreferencias/menuPreferencias.jsx
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import * as SharedStyle from '../../shared-style';
-
 import { Prefs } from './../../proxies/export';
 
 const STYLE = {
@@ -21,7 +19,6 @@ const BASE_STYLE_LABEL = {
   display: "block",
   fontSize: '0.75em',
   color: SharedStyle.PRIMARY_COLOR.master,
-  /*fontWeight: 'bold',*/
 };
 
 const STYLE_INPUT = {
@@ -39,99 +36,175 @@ const STYLE_INPUT = {
   width: '15%',
 };
 
-export default class MenuPreferencias extends Component {
+export default function MenuPreferencias ( props ) {
+  const [ state, setState ] = useState( {
+    prefs: [],
+    prefsState: {}
+  } );
 
-  constructor ( props ) {
-    super( props );
-    this.state = {
-      prefs: [],
-      prefsState: {},
-    };
-    this.onChange = this.onChange.bind( this );
-    this.onChangeCheckbox = this.onChangeCheckbox.bind( this );
-  }
+  useEffect( () => {
+    setState( { prefs: Prefs.getPrefsInfo(), prefsState: props.prefs } );
+  }, [] );
 
-  componentDidMount () {
-    /*console.log('prefsMethodOscar', Prefs.getPrefsInfo())*/
-    this.setState( {
-      prefs: Prefs.getPrefsInfo(),
-      prefsState: this.props.prefs,
-    } );
-  }
-
-  onChange ( name, value ) {
-    let newPrefsState = this.state.prefsState;
+  const onChange = ( name, value ) => {
+    const newPrefsState = state.prefsState;
     newPrefsState[ name ] = value;
 
-    this.setState( {
-      ...this.state,
-      prefsState: newPrefsState
-    } );
+    setState( ( prevState ) => (
+      { ...prevState, prefsState: newPrefsState }
+    ) );
 
-    this.props.projectActions.updatePreference( name, value );
+    props.projectActions.updatePreference( name, value );
+  };
 
+  return (
+    <div id={ 'menuPreferencias' } style={ STYLE }>
+      <div style={ { margin: '1em 2.5em' } }>
+        <h4>Menu de Preferencias</h4>
 
-    /*console.log('newPrefs', this.state.prefsState)*/
-  }
+        <div>
+          { state.prefs.map( ( pref, i ) => {
+            if ( pref.typeValor === 'Texto' ) {
+              return (
+                <div key={ i }>
+                  <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
+                    <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
+                    {/* TODO the only thing that changes is this snippet  */ }
 
-  onChangeCheckbox ( name, value ) {
-    let newPrefsState = this.state.prefsState;
-    newPrefsState[ name ] = value;
+                    <input style={ STYLE_INPUT } type={ 'text' }
+                      onChange={ ( e ) => onChange( pref.nombre, e.target.value ) }
+                      value={ state.prefsState[ pref.nombre ] }
+                      name={ pref.nombre }
+                    />
 
-    this.setState( {
-      ...this.state,
-      prefsState: newPrefsState
-    } );
+                    {/* TODO  */ }
+                    <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+                  </div>
+                </div> );
 
-    this.props.projectActions.updatePreference( name, value );
-
-    /*console.log('newPrefs', this.state.prefsState)*/
-  }
-
-  render () {
-    return (
-      <div id={ 'menuPreferencias' } style={ STYLE }>
-        <div style={ { margin: '1em 2.5em' } }>
-          <h4>Menu de Preferencias</h4>
-          <div>
-            {
-              this.state.prefs.map( ( pref, i ) => {
-                let valueInput = this.state.prefsState[ pref.nombre ];
-                if ( pref.typeValor === 'Texto' ) {
-                  return <div key={ i }>
-                    <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
-                      <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
-                      <input style={ STYLE_INPUT } type={ 'text' } onChange={ ( e ) => this.onChange( pref.nombre, e.target.value ) } value={ this.state.prefsState[ pref.nombre ] } name={ pref.nombre } />
-                      <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+            } else if ( pref.typeValor === 'Booleano' ) {
+              return (
+                <div key={ i }>
+                  <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
+                    <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
+                    <div style={ { display: 'flex' } }>
+                      <input style={ { ...STYLE_INPUT, width: '2em', margin: '0' } } type={ 'checkbox' }
+                        onChange={ ( e ) => onChange( pref.nombre, e.target.checked ) }
+                        checked={ state.prefsState[ pref.nombre ] }
+                        name={ pref.nombre }
+                      />
                     </div>
-                  </div>;
-                } else if ( pref.typeValor === 'Booleano' ) {
-                  return <div key={ i }>
-                    <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
-                      <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
-                      <div style={ { display: 'flex' } }>
-                        <input style={ { ...STYLE_INPUT, width: '2em', margin: '0' } } type={ 'checkbox' } onChange={ ( e ) => this.onChangeCheckbox( pref.nombre, e.target.checked ) } checked={ this.state.prefsState[ pref.nombre ] } name={ pref.nombre } />
-                      </div>
-                      <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
-                    </div>
-                  </div>;
-                } else if ( pref.typeValor === 'Entero' ) {
-                  return <div key={ i }>
-                    <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
-                      <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
-                      <input style={ STYLE_INPUT } type={ 'number' } onChange={ ( e ) => this.onChange( pref.nombre, e.target.value ) } value={ this.state.prefsState[ pref.nombre ] } name={ pref.nombre } />
-                      <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
-                    </div>
-                  </div>;
-                }
+                    <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+                  </div>
+                </div> );
 
-              } ) }
-          </div>
-        </div >
-      </ div >
-    );
-  }
-
-
-
+            } else if ( pref.typeValor === 'Entero' ) {
+              return (
+                <div key={ i }>
+                  <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
+                    <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
+                    <input style={ STYLE_INPUT } type={ 'number' }
+                      onChange={ ( e ) => onChange( pref.nombre, e.target.value ) }
+                      value={ state.prefsState[ pref.nombre ] }
+                      name={ pref.nombre }
+                    />
+                    <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+                  </div>
+                </div> );
+            }
+          } ) }
+        </div>
+      </div >
+    </ div >
+  );
 }
+
+// export default class MenuPreferencias extends Component {
+
+//   constructor ( props ) {
+//     super( props );
+//     this.state = {
+//       prefs: [],
+//       prefsState: {},
+//     };
+//     this.onChange = this.onChange.bind( this );
+//     this.onChangeCheckbox = this.onChangeCheckbox.bind( this );
+//   }
+
+//   componentDidMount () {
+//     this.setState( {
+//       prefs: Prefs.getPrefsInfo(),
+//       prefsState: this.props.prefs,
+//     } );
+//   }
+
+//   onChange ( name, value ) {
+//     let newPrefsState = this.state.prefsState;
+//     newPrefsState[ name ] = value;
+
+//     this.setState( {
+//       ...this.state,
+//       prefsState: newPrefsState
+//     } );
+
+//     this.props.projectActions.updatePreference( name, value );
+//   }
+
+//   onChangeCheckbox ( name, value ) {
+//     let newPrefsState = this.state.prefsState;
+//     newPrefsState[ name ] = value;
+
+//     this.setState( {
+//       ...this.state,
+//       prefsState: newPrefsState
+//     } );
+
+//     this.props.projectActions.updatePreference( name, value );
+//     /*console.log('newPrefs', this.state.prefsState)*/
+//   }
+
+//   render () {
+//     return (
+//       <div id={ 'menuPreferencias' } style={ STYLE }>
+//         <div style={ { margin: '1em 2.5em' } }>
+//           <h4>Menu de Preferencias</h4>
+//           <div>
+//             { this.state.prefs.map( ( pref, i ) => {
+//               // let valueInput = this.state.prefsState[ pref.nombre ];
+
+//               if ( pref.typeValor === 'Texto' ) {
+//                 return <div key={ i }>
+//                   <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
+//                     <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
+//                     <input style={ STYLE_INPUT } type={ 'text' } onChange={ ( e ) => this.onChange( pref.nombre, e.target.value ) } value={ this.state.prefsState[ pref.nombre ] } name={ pref.nombre } />
+//                     <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+//                   </div>
+//                 </div>;
+
+//               } else if ( pref.typeValor === 'Booleano' ) {
+//                 return <div key={ i }>
+//                   <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
+//                     <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
+//                     <div style={ { display: 'flex' } }>
+//                       <input style={ { ...STYLE_INPUT, width: '2em', margin: '0' } } type={ 'checkbox' } onChange={ ( e ) => this.onChangeCheckbox( pref.nombre, e.target.checked ) } checked={ this.state.prefsState[ pref.nombre ] } name={ pref.nombre } />
+//                     </div>
+//                     <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+//                   </div>
+//                 </div>;
+
+//               } else if ( pref.typeValor === 'Entero' ) {
+//                 return <div key={ i }>
+//                   <div style={ { display: 'flex', flexDirection: 'row', alignItems: 'center' } }>
+//                     <label style={ { ...BASE_STYLE_LABEL, margin: '20px', width: '7em' } }>{ pref.nombre }</label>
+//                     <input style={ STYLE_INPUT } type={ 'number' } onChange={ ( e ) => this.onChange( pref.nombre, e.target.value ) } value={ this.state.prefsState[ pref.nombre ] } name={ pref.nombre } />
+//                     <p style={ { ...BASE_STYLE_LABEL, marginLeft: '1.5em' } }>{ pref.descripcion }</p>
+//                   </div>
+//                 </div>;
+//               }
+//             } ) }
+//           </div>
+//         </div >
+//       </ div >
+//     );
+//   }
+// }
