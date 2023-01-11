3353dc07aaae00214aa114c09f48ae149ead16d4
Vikms95 <vms1995@hotmail.com>
2023-01-11 11:44:32 +0100

    format viewer3d

--- src/components/viewer3d/viewer3d.js
+++ src/components/viewer3d/viewer3d.js
@@ -12,12 +12,6 @@ import diff from 'immutablediff';
 import * as SharedStyle from '../../shared-style';
 import { dispatch3DZoomIn, dispatch3DZoomOut } from '../../utils/dispatch-event';
 import { getIsElementSelected } from '../../selectors/selectors';
-import nx from './1/nx.png';
-import ny from './1/ny.png';
-import nz from './1/nz.png';
-import px from './1/px.png';
-import py from './1/py.png';
-import pz from './1/pz.png';
 
 export default class Scene3DViewer extends Component {
 
@@ -72,11 +66,9 @@ export default class Scene3DViewer extends Component {
     let canvasWrapper = ReactDOM.findDOMNode( this.refs.canvasWrapper );
     let scene3D = new THREE.Scene();
 
-    //RENDERER
     this.renderer.setClearColor( new THREE.Color( SharedStyle.COLORS.white ) );
     this.renderer.setSize( this.width, this.height );
 
-    // ENCARGADOS DE RENDERIZAR LAS TEXTURAS DE ALTA CALIDAD
     this.renderer.physicallyCorrectLights = true;
     this.renderer.setPixelRatio( window.devicePixelRatio );
 
@@ -84,44 +76,27 @@ export default class Scene3DViewer extends Component {
     this.renderer.toneMapping = Number( THREE.LinearToneMapping );
     this.renderer.toneMappingExposure = Math.pow( 2, 0 );
 
-
-
-    // Add an environment with a set of PNGs
-    // const cubeTextureLoader = new THREE.CubeTextureLoader();
-    // const envMap = cubeTextureLoader.load( [
-    //   nx, ny, nz, px, py, pz
-    // ] );
-
-    // scene3D.environment = envMap;
-
-    // Add a generated environment map with a PMREMGenerator
     const environment = new RoomEnvironment();
     const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
     scene3D.environment = pmremGenerator.fromScene( environment, 0.04 ).texture;
 
-    // LOAD DATA
-    let planData = parseData( state, data, actions, this.context.catalog );
+    const planData = parseData( state, data, actions, this.context.catalog );
     scene3D.add( planData.plan );
     scene3D.add( planData.grid );
 
-    let aspectRatio = this.width / this.height;
-    let camera = new THREE.PerspectiveCamera( 45, aspectRatio, 1, 300000 );
+    const aspectRatio = this.width / this.height;
+    const camera = new THREE.PerspectiveCamera( 45, aspectRatio, 1, 300000 );
 
     scene3D.add( camera );
 
-    // Set position for the camera
-    let cameraPositionX = -( planData.boundingBox.max.x - planData.boundingBox.min.x ) / 2;
-    let cameraPositionY = ( planData.boundingBox.max.y - planData.boundingBox.min.y ) / 2 * 10;
-    let cameraPositionZ = ( planData.boundingBox.max.z - planData.boundingBox.min.z ) / 2;
+    const cameraPositionX = -( planData.boundingBox.max.x - planData.boundingBox.min.x ) / 2;
+    const cameraPositionY = ( planData.boundingBox.max.y - planData.boundingBox.min.y ) / 2 * 10;
+    const cameraPositionZ = ( planData.boundingBox.max.z - planData.boundingBox.min.z ) / 2;
 
     camera.position.set( cameraPositionX, cameraPositionY, cameraPositionZ );
     camera.up = new THREE.Vector3( 0, 1, 0 );
 
-    // HELPER AXIS
-    //let axisHelper = new Three.AxisHelper( 100 );
-    //scene3D.add( axisHelper );
 
-    // LIGHT;
     // let light = new THREE.AmbientLight( 0xafafaf ); // soft white light
     // scene3D.add( light );
 
@@ -131,11 +106,7 @@ export default class Scene3DViewer extends Component {
     // spotLight1.position.set( cameraPositionX, cameraPositionY, cameraPositionZ );
     // scene3D.add( spotLight1 );
 
-    // Light Helper
-    //const spotLightHelper = new Three.SpotLightHelper( spotLight1 );
-    //scene3D.add( spotLightHelper );
 
-    // OBJECT PICKING
     let toIntersect = [ planData.plan ];
     let mouse = new THREE.Vector2();
     let raycaster = new THREE.Raycaster();
