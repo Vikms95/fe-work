a43e5026488a9d05fa19b043c508f89e5c797d74
Vikms95 <vms1995@hotmail.com>
2023-01-13 22:03:08 +0100

    refactor useDrawMode

--- src/components/menu-rooms/menu-rooms.jsx
+++ src/components/menu-rooms/menu-rooms.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MODE_DRAWING_LINE, MODE_WAITING_DRAWING_LINE } from '../../constants';
 import * as SharedStyle from '../../shared-style';
 import './style.css';
 
--- src/hooks/useDrawMode.js
+++ src/hooks/useDrawMode.js
@@ -2,20 +2,26 @@ import { useEffect, useState } from 'react';
 import { MODE_DRAWING_LINE, MODE_WAITING_DRAWING_LINE } from '../constants';
 
 export function useDrawMode ( state ) {
+  const drawMode = state.getIn( [ 'react-planner', 'mode' ] ) === MODE_DRAWING_LINE;
+  const waitingDrawMode = state.getIn( [ 'react-planner', 'mode' ] ) === MODE_WAITING_DRAWING_LINE;
+
   const [ isDrawMode, setIsDrawMode ] = useState( false );
-  const isDrawCategoryMode = () => (
-    state.getIn( [ 'react-planner', 'mode' ] ) === MODE_WAITING_DRAWING_LINE ||
-    state.getIn( [ 'react-planner', 'mode' ] ) === MODE_DRAWING_LINE
-  );
 
   useEffect( () => {
-    if ( isDrawCategoryMode() )
+    console.log( "entering useffect" );
+    if ( drawMode || waitingDrawMode ) {
+      console.log( "draw mode" );
       setIsDrawMode( true );
 
-    else
+    }
+
+    else {
+      console.log( "no draw mode" );
       setIsDrawMode( false );
 
-  }, [ state ] );
+    }
+
+  }, [ drawMode, waitingDrawMode ] );
 
   return isDrawMode;
 }
